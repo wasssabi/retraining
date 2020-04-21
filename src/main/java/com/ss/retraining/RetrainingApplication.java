e04aa2ef26d54322094fb9b39f41a25a6099719f
wasssabi <nazarlucholat@gmail.com>
2020-04-21 11:53:26 +0300

    Test

---
+++ src/main/java/com/ss/retraining/RetrainingApplication.java
@@ -0,0 +1,13 @@
+package com.ss.retraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ss/retraining/RetrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ss.retraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
